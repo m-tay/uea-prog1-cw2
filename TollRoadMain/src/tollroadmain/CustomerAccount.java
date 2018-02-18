36eae4da81cab304f9a03a032b32fb55f1be2440
Matthew Taylor <ecstatic@gmail.com>
2018-02-18 12:12:49 +0000

    Finished implementing CustomerAccount class
    - Implemented constructuor
    - Implemented Comparable
    - Started making tests

--- TollRoadMain/src/tollroadmain/CustomerAccount.java
+++ TollRoadMain/src/tollroadmain/CustomerAccount.java
@@ -1,6 +1,6 @@
 package tollroadmain;
 
-public class CustomerAccount { // TODO implement Comparable
+public class CustomerAccount implements Comparable<CustomerAccount> { 
     private enum Discount {NONE, STAFF, FRIENDS_AND_FAMILY}
     
     private String firstName;
@@ -9,7 +9,13 @@ public class CustomerAccount { // TODO implement Comparable
     private Discount discount;
     private Vehicle vehicle;
     
-    // TODO implement constructor
+    // Constructor
+    public CustomerAccount(String firstName, String lastName, Vehicle vehicle) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        balance = 0; // Assuming accounts always start with no balance
+        this.vehicle = vehicle;
+    }
     
     public void activateStaffDiscount() {
         discount = Discount.STAFF;
@@ -38,5 +44,50 @@ public class CustomerAccount { // TODO implement Comparable
         return cost;
     }
     
+    public int compareTo(CustomerAccount other) {
+        return vehicle.regPlate.compareTo(other.vehicle.regPlate);
+    }
+    
+    // Accessor methods
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public int getBalance() {
+        return balance;
+    }
+    
+    // Test harness
+    public static void main(String args[]) {
+        Car testCar1 = new Car("AA11 AAA", "Ford", 5);  
+        CustomerAccount testAcc1 = new CustomerAccount("Joe", "Bloggs", testCar1);
+        
+        // Testing accessor methods
+        System.out.println("testAcc1 first name: " + testAcc1.firstName);
+        System.out.println("testAcc1 last name : " + testAcc1.lastName);
+        System.out.println("testAcc1 balance   : " + testAcc1.balance);
+        
+        // Testing compareTo implementation
+        Car testCar2 = new Car("BB22 BBB", "Vauxhall", 3);  
+        CustomerAccount testAcc2 = new CustomerAccount("Steve", "McQueen", testCar2);
+        
+        Car testCar3 = new Car("AA11 AAA", "Seat", 3);  
+        CustomerAccount testAcc3 = new CustomerAccount("Donald", "Knuth", testCar3);
+
+        // Should return -1 (first reg < other reg)
+        System.out.println("testAcc1 compared to testAcc2 : " + testAcc1.compareTo(testAcc2));
+        
+        // Should return 0 (first reg == other reg)
+        System.out.println("testAcc1 compared to testAcc3 : " + testAcc1.compareTo(testAcc3));
+        
+        // Should return 1 (first reg > other reg)
+        System.out.println("testAcc2 compared to testAcc1 : " + testAcc2.compareTo(testAcc1));
+        
+        
+    }
 }
 
